fef2cfe990e5418ad2671571f270fa8974d67eb7
Uarsa <59147934+Uarsa@users.noreply.github.com>
2022-02-07 11:29:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
-import datetime
-import os
-
-
-#UPLOAD_FOLDER = "data/data"
-#ALLOWED_EXTENSIONS = {'txt', 'py', }
 
 
 app = Flask(__name__)
-#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 
 count = 0
 @app.route('/', methods=['POST', 'GET'])
@@ -21,6 +13,4 @@ def index():
   else:
     return render_template('index.html', count=count)
 
-  
-  
   
