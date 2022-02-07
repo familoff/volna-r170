26c7a898ec5e8949d0b600f9067711d8d6cc5185
Uarsa <59147934+Uarsa@users.noreply.github.com>
2022-02-07 11:46:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+count = 0
 @app.route('/', methods=['POST', 'GET'])
 def index():
+  global count
   if request.method == "POST":
-    
+    count += 1
     return redirect('/')
   else:
-    return render_template('index.html')
+    return render_template('index.html', count=count)
 
   
