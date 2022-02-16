8580b11b78990db9c43917201cec6e9cea714b75
Uarsa <59147934+Uarsa@users.noreply.github.com>
2022-02-16 08:49:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -540,7 +540,7 @@ def table_6(t1, t2):
   tab_6_1 = []
   
   for i in range(len(volumes_1)):
-    tab_6_1.append(volumes_1[i], volumes_2[i])
+    tab_6_1.append([volumes_1[i], volumes_2[i]])
     
     
   return tab_6, tab_6_1
